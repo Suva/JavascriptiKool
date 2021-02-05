a9aa4f6d99542bbc76a99e1d4b64bff9a2f60d51
Jaan Pullerits <clysuva@gmail.com>
2021-02-05 17:08:52 +0200

    Add logic info

---
+++ elseif.js
@@ -0,0 +1,21 @@
+if (1 == 1) {
+    console.log("if", 1)
+}
+
+if (2 == 2) {
+    console.log("if", 2)
+}
+
+if (3 == 3) {
+    console.log("if", 3)
+}
+
+if (1 == 1) {
+    console.log("elseif", 1)
+} else if (2 == 2) {
+    console.log("elseif", 2)
+} else if (3 == 3) {
+    console.log("elseif", 3)
+} else {
+    console.log("else")
+}
---
+++ logic_operators.js
@@ -0,0 +1,16 @@
+if (1 == 1 && 1 == 2) {
+    console.log("true & false")
+}
+
+if (1 == 1 && 2 == 2) {
+    console.log("true & true")
+}
+
+if (1 == 1 || 1 == 2) {
+    console.log("true | false")
+}
+
+if (1 == 3 || 1 == 2) {
+    console.log("false | false")
+}
+
