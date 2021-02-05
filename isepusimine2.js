0d34dee765990c0215cfd6dd3ee717f7f1062717
aboutannika <aboutannika@gmail.com>
2021-02-05 16:34:56 +0200

    readline-sync abil loopid, if/else tingimused jms harjutamine

--- isepusimine2.js
+++ isepusimine2.js
@@ -2,7 +2,7 @@ const reader = require('readline-sync')
 
 while (true) {
 
-let p6hikysimus = reader.question("Kui palju sa palka saad muidu? ")
+    let p6hikysimus = reader.question("Kui palju sa palka saad muidu? ")
 
     let palk = parseInt(p6hikysimus)
 
@@ -13,20 +13,15 @@ let p6hikysimus = reader.question("Kui palju sa palka saad muidu? ")
 
     if (palk < 500) {
         console.log("Paras rott! Kelle kulul sa elad?")
-    }
-    else if (palk > 500, palk < 1000) {
+    } else if (palk > 500, palk < 1000) {
         console.log("Saad ikka hakkama vä?")
-    }
-    else if (palk > 1000, palk < 1500) {
+    } else if (palk > 1000, palk < 1500) {
         console.log("Ei noh, Eesti keskmine on ka tore olla.")
-    }
-    else if (palk > 1500, palk < 2500) {
+    } else if (palk > 1500, palk < 2500) {
         console.log("Zajebis ju.")
-    }
-    else if (palk > 2500, palk < 10000) {
+    } else if (palk > 2500, palk < 10000) {
         console.log("Davai, tee laenu, ahv!")
-    }
-    else if (palk >= 10000) {
+    } else if (palk >= 10000) {
         console.log("Ah, hui usungi! Valevorst, raisk.")
         break
     }
