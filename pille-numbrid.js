93a5980765d4148fa20946294fda2bda438dcb46
Jaan Pullerits <clysuva@gmail.com>
2021-02-05 17:09:12 +0200

    Rename Pilles homework

--- pille-numbrid.js
+++ pille-numbrid.js
@@ -10,19 +10,16 @@ while (true) {
         break
     }
 
-
     if (palk < 500) {
         console.log("Paras! Kelle kulul sa elad?")
-
-    } else if (palk > 500, palk < 1000) {
-        console.log("Kuidas sa hakkama saad?")
-    } else if (palk > 1000, palk < 1500) {
-        console.log("Vapsjee rikkur, anna pappi!")
-    } else if (palk > 1500, palk < 2500) {
-        console.log("OMG, anneta kassidele siis.")
     } else if (palk > 2500) {
         console.log("Tere, härra Urmas Sõõrumaa.")
-        break
+    } else if (palk > 1500) {
+        console.log("OMG, anneta kassidele siis.")
+    } else if (palk > 1000) {
+        console.log("Vapsjee rikkur, anna pappi!")
+    } else if (palk > 500) {
+        console.log("Kuidas sa hakkama saad?")
     }
 
 }
